122b97e01460068abc48ef3d2d3667e56bfd4846
Necryotiks <eman13vil@gmail.com>
2018-04-28 02:21:44 -0700

    Added comments.

--- OBJ0:Shaloam_Tensorflow.py
+++ OBJ0:Shaloam_Tensorflow.py
@@ -1,4 +1,5 @@
+#This is a very simple object designed to test if Tensorflow installed correctly. Nice and easy.
 import tensorflow as tf
-shaloam = tf.constant('Shaloam, Tensorflow!')
-session = tf.Session()
-print(session.run(shaloam))
+shaloam = tf.constant('Shaloam, Tensorflow!') #NOTE0: Declaring a variable/constant/whatever in TF is not done through '=' assignment operator.
+session = tf.Session() #NOTE1: All models in TF require a session in order to execute them.
+print(session.run(shaloam)) #"Executing" a graph.
