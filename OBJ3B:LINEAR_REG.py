ea8c3f08e3b602aef308713800e1f5798910cddd
CaseyHunter <cmhunter@protonmail.ch>
2018-05-10 17:06:06 -0700

    Hi!

--- OBJ3B:LINEAR_REG.py
+++ OBJ3B:LINEAR_REG.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 import numpy as np
-import utils
 
 DATA_FILE = "datasets/birthrate.txt"
 
 # Step 1: read in data from the .txt file
 # data is a numpy array of shape (190, 2), each row is a datapoint
 #TODO: fix this
-data= np.loadtxt(DATA_FILE,skiprows=1,usecols=(1,2))
-
+data= np.loadtxt(DATA_FILE,skiprows=1,usecols=(1,2)).astype(np.float32)
+dataset = tf.data.Dataset.from_tensor_slices((data[:,0],data[:,1]))
 # Step 2: create placeholders for X (birth rate) and Y (life expectancy)
 X = tf.placeholder(tf.float32, name='X')
 Y = tf.placeholder(tf.float32, name='Y')
@@ -19,7 +18,7 @@ u = tf.get_variable('weights2', initializer=tf.constant(0.0))
 b = tf.get_variable('bias', initializer=tf.constant(0.0))
 
 # Step 4: construct model to predict Y (life expectancy from birth rate)
-Y_predicted = ((w *X*X) + (X*u) + b) 
+Y_predicted = ((w *X) + b) 
 
 # Step 5: use the square error as the loss function
 loss = tf.square(Y - Y_predicted, name='loss')
@@ -35,14 +34,20 @@ optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss
 
 with tf.Session() as sess:
     # Step 7: initialize the necessary variables, in this case, w and b
-    sess.run(tf.global_variables_initializer()) 
+    iterator = dataset.make_initializable_iterator()
+    sess.run(tf.global_variables_initializer())
+    X,Y= iterator.get_next()
 
-    # Step 8: train the model
+
+      # Step 8: train the model
     for i in range(100): # run 100 epochs
-        for x, y in data:
             # Session runs train_op to minimize loss
-            sess.run(optimizer, feed_dict={X: x, Y:y}) 
-
+	sess.run(iterator.initializer)
+	try:
+		while True:
+            		sess.run([optimizer]) 
+	except tf.errors.OutOfRangeError:
+		pass
     # Step 9: output the values of w and b
     w_out, b_out = sess.run([w, b])
     print(w_out,b_out)
